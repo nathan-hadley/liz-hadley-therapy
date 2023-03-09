81c0dfb2d954013b264af428da717444c468823e
Nathan Hadley <nathan.hadley@me.com>
2023-03-09 08:36:09 -0700

    bug fixes

--- app/controllers/areas_of_focus_controller.rb
+++
@@ -1,3 +0,0 @@
-class AreasOfFocusController < ApplicationController
-  def index; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources 'home', only: :index
   resources 'about', only: :index
-  get 'areas-of-focus', to: 'areas_of_focus#index', as: 'areas_of_focus'
+  get 'focus-areas', to: 'focus_areas#index', as: 'focus_areas'
   resources 'faq', only: :index
   resources 'resources', only: :index
   resources 'contact', only: :index
